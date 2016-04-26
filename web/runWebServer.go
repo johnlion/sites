b6b6b4c073b8559200dc8254b19e78ce2fbfd5b8
johnlion <xps_8@hotmail.com>
2016-04-26 15:32:44 +0800

    功能:运行并监听HTTP服务器

---
+++ web/runWebServer.go
@@ -0,0 +1,25 @@
+package web
+
+import (
+	"net/http"
+	"log"
+)
+/*********************************************
+ * Author: chandlerxue
+ * Email: xps_8@hotmail.com
+ * Date: 2016年4月25日 上午11:09
+ * Func: RunWebServer
+ * Desc: web package entry  运行并监听
+ *********************************************/
+func ( w *Web ) RunWebServer( ){
+	//http.ListenAndServe( DOMAIN_ADDR ,  w )
+	w.Server = &http.Server{
+		Addr:           DOMAIN_ADDR,
+		Handler:        w,
+		ReadTimeout:    HTTP_READ_TIME_OUT,
+		WriteTimeout:   HTTP_WRITE_TIME_OUT,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(w.Server.ListenAndServe())
+
+}
--- web/web.go
+++ web/web.go
@@ -3,8 +3,27 @@ package web
 import (
 	"flag"
 	"net/http"
-	"log"
 )
+
+/*********************************************
+ * Author: chandlerxue
+ * Email: xps_8@hotmail.com
+ * Date: 2016年4月26日 下午3:28
+ * File: web.go
+ * Desc: web类 处理http相关业务
+ * Example:
+	import (
+		"github.com/johnlion/sites/web"
+		"fmt"
+
+	)
+	func main(){
+		ObjWeb := web.Web_constract()
+		ObjWeb.RunWebServer()
+		fmt.Println( *ObjWeb.Domain )
+	}
+
+ *********************************************/
 
 type Web struct {
 	Domain *string
@@ -37,27 +56,6 @@ func Web_constract() *Web{
 
 	return &web
 }
-
-/*********************************************
- * Author: chandlerxue
- * Email: xps_8@hotmail.com
- * Date: 2016年4月25日 上午11:09
- * Func: RunWebServer
- * Desc: web package entry  运行并监听
- *********************************************/
-func ( w *Web ) RunWebServer( ){
-	//http.ListenAndServe( DOMAIN_ADDR ,  w )
-	w.Server = &http.Server{
-		Addr:           DOMAIN_ADDR,
-		Handler:        w,
-		ReadTimeout:    HTTP_READ_TIME_OUT,
-		WriteTimeout:   HTTP_WRITE_TIME_OUT,
-		MaxHeaderBytes: 1 << 20,
-	}
-	log.Fatal(w.Server.ListenAndServe())
-
-}
-
 
 func (w *Web ) SetHeader ( header map[string]string ){
 	w.Header =  header
