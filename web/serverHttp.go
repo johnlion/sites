3cf0473cc581480db5fd88faa7b0a4798198bb86
johnlion <xps_8@hotmail.com>
2016-04-26 15:26:20 +0800

    Http服务器 Response, Request

---
+++ web/serverHttp.go
@@ -0,0 +1,27 @@
+package web
+
+import (
+	"net/http"
+	"fmt"
+)
+/*********************************************
+ * Author: chandlerxue
+ * Email: xps_8@hotmail.com
+ * Date: 2016年4月25日 下午1:37
+ * Func: ServeHTTP
+ * Desc:
+ *********************************************/
+func ( w *Web ) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+	/* 设置 Response Header */
+	for i,v := range w.Header{
+		res.Header().Set( i, v )
+	}
+
+	/* web 输出  */
+	fmt.Fprint( res, "This is a test" )
+	w.Debug( "This is a test" )
+	w.Log(  res.Header().Get( "User-Agent" ) )
+	/* command 输出 */
+
+}
--- web/web.go
+++ web/web.go
@@ -3,7 +3,6 @@ package web
 import (
 	"flag"
 	"net/http"
-	"fmt"
 	"log"
 )
 
@@ -59,27 +58,6 @@ func ( w *Web ) RunWebServer( ){
 
 }
 
-/*********************************************
- * Author: chandlerxue
- * Email: xps_8@hotmail.com
- * Date: 2016年4月25日 下午1:37
- * Func: ServeHTTP
- * Desc:
- *********************************************/
-func ( w *Web ) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-
-	/* 设置 Response Header */
-	for i,v := range w.Header{
-		res.Header().Set( i, v )
-	}
-
-	/* web 输出  */
-	fmt.Fprint( res, "This is a test" )
-	w.Debug( "This is a test" )
-	w.Log(  res.Header().Get( "User-Agent" ) )
-	/* command 输出 */
-
-}
 
 func (w *Web ) SetHeader ( header map[string]string ){
 	w.Header =  header
