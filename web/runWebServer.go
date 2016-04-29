cdd67736232bedf5bb9fb69e0742b65f846291ef
johnlion <xps_8@hotmail.com>
2016-04-29 15:20:43 +0800

    同时兼听多个服务器

--- web/runWebServer.go
+++ web/runWebServer.go
@@ -23,14 +23,16 @@ func ( w *Web ) RunWebServer( ){
 		WriteTimeout:   config.HTTP_WRITE_TIME_OUT,
 		MaxHeaderBytes: 1 << 20,
 	}
-
 	*/
+
+	/* 多服务器组设置  */
 	w.Server =  map[int]*http.Server{
 		1: &http.Server{ Addr: config.DOMAIN_ADDR, Handler: w,ReadTimeout:    config.HTTP_READ_TIME_OUT, WriteTimeout:   config.HTTP_WRITE_TIME_OUT, MaxHeaderBytes: 1 << 20,  },
 		2: &http.Server{ Addr: "127.0.0.1:9091", Handler: w,ReadTimeout:    config.HTTP_READ_TIME_OUT, WriteTimeout:   config.HTTP_WRITE_TIME_OUT, MaxHeaderBytes: 1 << 20,},
 		3: &http.Server{ Addr: "127.0.0.1:9092", Handler: w,ReadTimeout:    config.HTTP_READ_TIME_OUT, WriteTimeout:   config.HTTP_WRITE_TIME_OUT, MaxHeaderBytes: 1 << 20,},
 	}
 
+	/* 协程挂起服务器 */
 	done := make(chan bool)
 	for key,val := range w.Server{
 		go w.Debug( "Server [" +  strconv.Itoa(key) + "] Start Listen And Server ...... " )
